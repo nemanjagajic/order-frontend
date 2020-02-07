6763f03e10922d071ac96a79b05aec91f7c5ef22
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-02-07 17:07:50 +0100

    minor refactor

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -4,13 +4,12 @@ import { useSelector } from 'react-redux'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const token = useSelector(state => state.auth.user.token)
-  const isLoggedIn = token !== null
 
   return (
     <Route
       {...rest}
       render={props =>
-        isLoggedIn ? (
+        token ? (
           <Component {...props} />
         ) : (
           <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />
